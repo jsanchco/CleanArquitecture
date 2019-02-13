b5f395112dd44e1e162aa7d32f89457d2c8a14cc
jsanchco-WORK <jsanchco@gmail.com>
2019-02-13 15:39:46 +0100

    jsanchez-WORK. Add component to client angular

--- Solution/CA.PeopleWEB/PeopleClient/src/app/app-routing.module.ts
+++ Solution/CA.PeopleWEB/PeopleClient/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
